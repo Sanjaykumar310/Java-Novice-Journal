533c24ffb51def02bf3802e4434ea79466220021
Sanjaykumar310 <222274132+Sanjaykumar542@users.noreply.github.com>
2025-07-28 20:20:18 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,15 +3,7 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char choice;::
-
-        
-                op == '*' ? a * b :
-                op == '/' ? (b != 0 ? a / b : "Error: Cannot divide by zero") :
-                "Error: Invalid operator"
-            ));
-
-            System.out.print("Do you want to calculate again? (y/n): ");
+        nt("Do you want to calculate again? (y/n): ");
             choice = sc.next().charAt(0);
 
         } while (choice == 'y' || choice == 'Y');
