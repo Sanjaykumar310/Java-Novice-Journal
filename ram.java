080abb412cb956a8fbd175081d33e200dfdc61f5
Sanjaykumar310 <222274132+Sanjaykumar542@users.noreply.github.com>
2025-07-31 19:40:23 +0530

    updated version

---
+++ ram.java
@@ -0,0 +1,14 @@
+class ram{
+     public static void main(String[] args){
+        int num = 103;
+
+        if(num%2==0){
+            System.out.println("enter a even number");
+
+        }
+        else{
+            System.out.println("enter a odd number");
+        }
+    
+    }
+}
