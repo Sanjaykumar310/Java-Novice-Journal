20f468f47d864dcf43e945f06f53772734a2572c
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-05-23 11:37:09 +0530

    added some files

---
+++ Casting.java
@@ -0,0 +1,11 @@
+class Casting{
+    public static void main(String[] args) {
+        double d = 9.99;
+        int i  = 65;
+        int x = (int)d;
+        char c = (char)i;
+        System.out.println(x);
+        System.out.println(i);
+        System.out.println(c);
+    }
+}
