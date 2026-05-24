9b00254796f2bda81d1ce1b331b8ec0705202684
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-05-24 22:23:29 +0530

    ADDEDSOMECODES

---
+++ GradeAverage.java
@@ -0,0 +1,8 @@
+class GradeAverage{
+    public static void main(String[] args) {
+        int m1 = 85 , m2 = 90 , m3 = 78 , m5 = 88;
+
+        double avg =(double)(m1+m2+m3+m5)/5;
+        System.out.println("AverageGrade : "+avg);
+    }
+}
