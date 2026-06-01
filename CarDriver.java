fe1f556f452da22767a3f71b06abf26a53c78b61
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-01 23:28:37 +0530

    added some codes to built object creation

---
+++ CarDriver.java
@@ -0,0 +1,8 @@
+class CarDriver{
+    public static void main(String[] args) {
+        car c1 =new car();
+        c1.showDetails();
+        car c2 = new car("apple",120);
+        c2.showDetails();
+    }
+}
