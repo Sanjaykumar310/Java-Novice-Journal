998b2231d0e00467af4781a9eef7a96e74602f17
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-02 05:53:41 +0530

    added some basic codes

---
+++ Bool.java
@@ -0,0 +1,12 @@
+//Q9. Create a method isEven that takes an integer and returns true if even, false if odd.
+
+class Bool {
+   public void isEven(int a){
+        if(a/0 == 0 ){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
---
+++ Demo.java
@@ -0,0 +1,7 @@
+//Q8. Create a method called add that takes two integers and returns their sum. Call it from main.
+class Demo{
+
+    public int add(int a,int b){
+        return a+b;
+    }
+}
