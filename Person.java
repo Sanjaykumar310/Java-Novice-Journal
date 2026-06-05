783e0167b27ee1d3e5a85f16f19d7e4c978e9b8a
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-05 06:10:41 +0530

    addedsomecode

---
+++ Person.java
@@ -0,0 +1,24 @@
+class Person{
+    String name;
+    int age;
+    String city;
+
+    public Person(String name) {
+        this.name=name;
+    }
+
+    public Person(String name ,int age) {
+        this.name = name;
+        this.age=age;
+
+    }
+
+    public Person(String name,int age,String city) {
+        this.name = name;
+        this.age=age;
+        this.city=city;
+    }
+    
+
+    
+}
