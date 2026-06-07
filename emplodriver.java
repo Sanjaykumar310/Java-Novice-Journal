93c41563509cd0698a400a0b0502db0e1d35490b
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-07 23:00:36 +0530

    addedsomecode

---
+++ emplodriver.java
@@ -0,0 +1,8 @@
+class emplodriver{
+    public static void main(String[] args) {
+        emplo e1 =new  emplo();
+        e1.emplodetails();
+
+        //System.out.println(e1.emplodetails());
+    }
+}
