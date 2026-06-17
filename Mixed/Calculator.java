1e10d3564258c2e426a41fa1e14853dda0962239
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-17 07:13:05 +0530

    added some code

---
+++ Mixed/Calculator.java
@@ -0,0 +1,22 @@
+class Calculator{
+
+
+    public void area(double radius){
+        System.out.println("circle area");
+    }
+    public void area(double length,double width){
+        System.out.println("rectangle area");
+    }
+    public void area(double base,double height,boolean isTriangle){
+        System.out.println("triangle area");
+    }
+}
+
+class Calculatordriver{
+    public static void main(String[] args) {
+        Calculator c1 = new Calculator();
+        c1.area(45);
+        c1.area(45, 78);
+        c1.area(42, 45, true);
+    }
+}
