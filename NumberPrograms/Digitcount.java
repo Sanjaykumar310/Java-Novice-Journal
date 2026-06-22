9183da0b89601e91cd0068b86d78ebac9bcb44af
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-22 21:28:06 +0530

    added some codes

--- NumberPrograms/Digitcount.java
+++ NumberPrograms/Digitcount.java
@@ -7,7 +7,7 @@ class Digitcount{
         Scanner sc = new Scanner(System.in);//getting input from user
         int input = sc.nextInt();//getting input from user
         
-        int temp  = input;//789
+       // int temp  = input;//789
         int count = 0;      //initial stage 0,1,2,3
         while(input > 0){   //check the conditions and to find the digit we use the while loops
             input = input/10;   //789/10=78,78/10=7,7/10=0
