51090f55102d1c09c0aef18b82a894370083cdfb
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-22 19:16:16 +0530

    addedsomecodes

---
+++ NumberPrograms/Sumofdigits.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+class Sumofdigits{
+    public static void main(String[] args) {
+        System.out.println("ENTER YOUR INPUTS NUMBER:");
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+    }
+}
