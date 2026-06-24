d1959bdeea0cbc4929a4f72b18d3c643e4e28e3e
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-24 18:17:35 +0530

    added some code

--- NumberPrograms/Factorial.java
+++ NumberPrograms/Factorial.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 class Factorial{
     public static void main(String[] args) {
-        System.out.println("ENTER YOUR NUMBER :");
+        System.out.print("ENTER YOUR NUMBER :");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int fact = number;
-        int i ;
-        while(number > 0){
+        int fact = 1;
+       for (int i = 1;i <= number ;i++) {
             fact = fact * i;
-        }
-        System.out.println(fact);
+           
+       }
+       System.out.println("FACTORIAL NUMBER :"+fact);
     }
 }
