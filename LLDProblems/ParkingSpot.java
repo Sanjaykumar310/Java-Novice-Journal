f8a3f836b8a0b7c63114a712e9b29bfd313b5458
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-26 21:59:55 +0530

    addedsomecodes

---
+++ LLDProblems/ParkingSpot.java
@@ -0,0 +1,13 @@
+class ParkingSpot{
+    boolean isOccupied;
+    int spotnumber;
+    Vehicle vehicle; //stores the object 
+
+
+   public void  parkVehicle(Vehicle v){
+
+    }
+    public void removeVehicle(){
+        
+    }
+}
