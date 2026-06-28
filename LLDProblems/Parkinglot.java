685e591faf58023aeb63adf6db7ff9bf3ca75774
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-06-28 13:22:37 +0530

    addedsomecodes

--- LLDProblems/Parkinglot.java
+++ LLDProblems/Parkinglot.java
@@ -3,10 +3,57 @@ A parking lot has multiple spots. Spots can hold either a Car, Bike, or Truck. E
 
 class Parkinglot{
     private ParkingSpot[] spots;
+    private int size;
 
     public Parkinglot(int size){
-        ParkingSpot spots = new ParkingSpot[10];
-        
+        this.size = size;
+        spots = new ParkingSpot[size];
+        for (int i = 0; i < size; i++) {
+            spots[i] = new ParkingSpot(i + 1);
+        }
+    }
+
+    public int findAvailableSpot() {
+        for (int i = 0; i < size; i++) {
+            if (spots[i] == null || !isSpotOccupied(spots[i])) {
+                System.out.println("Available spot: " + (i + 1));
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isSpotOccupied(ParkingSpot spot) {
+        if (spot == null) {
+            return false;
+        }
+
+        try {
+            java.lang.reflect.Method m = ParkingSpot.class.getMethod("isOccupied");
+            Object result = m.invoke(spot);
+            if (result instanceof Boolean) {
+                return (Boolean) result;
+            }
+        } catch (Exception e) {
+        }
+
+        try {
+            java.lang.reflect.Method m = ParkingSpot.class.getMethod("isEmpty");
+            Object result = m.invoke(spot);
+            if (result instanceof Boolean) {
+                return !((Boolean) result);
+            }
+        } catch (Exception e) {
+        }
+
+        try {
+            java.lang.reflect.Method m = ParkingSpot.class.getMethod("getVehicle");
+            Object result = m.invoke(spot);
+            return result != null;
+        } catch (Exception e) {
+        }
+
+        return false;
     }
 }
     
