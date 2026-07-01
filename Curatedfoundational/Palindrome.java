817fd511007a116da4f147ca943d6bf2e933851c
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-07-01 07:38:01 +0530

    addedsomecode

--- Curatedfoundational/Palindrome.java
+++ Curatedfoundational/Palindrome.java
@@ -1,14 +1,20 @@
+
+import java.util.Scanner;
+
 /*9. Palindrome Check
 Write a method isPalindrome(int n) that checks if a number is a palindrome (e.g., 121 → true). */
 class Palindrome{
-    public int isPalindrome(int number){
+    public int isPalindrome(){
+        System.out.print("ENTER YOUR INPUT:");
+        Scanner sc = new Scanner(System.in);
         
-        int original = 
+        int original = 0;
         int digit =0;
+        int number = sc.nextInt();
         while(number > 0){
             
              digit = number % 10;//exctract the last digit
-             original = number * 10 + digit;//to reverse it
+             original = original * 10 + digit;//to reverse it
              number = number / 10;//to remove the last digits
         
         }
@@ -18,7 +24,12 @@ class Palindrome{
         else{
             System.out.println("not palindrome");
         }
-        return number;
+        return boolean;
+    }
+    public static void main(String[] args) {
+        Palindrome p = new Palindrome();
+        p.isPalindrome();
+       
     }
     
 }
