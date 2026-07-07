bfe311e33e5ba12b8625bb758b3a59c888af0c4f
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-07-07 22:44:36 +0530

    addedsomecodes

---
+++ Arrays/ArraySum.java
@@ -0,0 +1,12 @@
+//Create an integer array and find the sum of all elements.
+class ArraySum{
+    public static void main(String[] args) {
+        int number[] = {1,2,3,4,5};
+        for(int i = 0;i <= number.length;i++){
+            for(int j = 0; j <= number.length ; j++){
+                System.out.println(i+j);
+
+            }
+        }
+    }
+}
