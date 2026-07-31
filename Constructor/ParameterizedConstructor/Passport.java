fdda874e65619eff67818f3e1bdd5570ee222d92
Sanjay Kumar <105733558+Sanjaykumar310@users.noreply.github.com>
2026-07-31 13:11:12 +0530

    addedsomefiles

--- Constructor/ParameterizedConstructor/Passport.java
+++ Constructor/ParameterizedConstructor/Passport.java
@@ -5,8 +5,19 @@ class Passport{
         String nationality;
         int expiryYear;
 
+        Passport(String passportNumber,String holderName,String nationality,int expiryYear){
+            this.passportNumber = passportNumber;
+            this.holderName = holderName;
+            this.nationality = nationality;
+            int expiryYear = expiryYear; 
+        }
+
+        void displayPassport(){
+            syste
+        }
+
     public static void main(String[] args) {
         
-        
+
     }
 }
